ee908ab97e7d7a3d301073c0d3e1fca1b245abe0
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-15 10:22:40 +0100

    Adds contactinfo

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@
 import css from 'next/css'
 
 export default () => (
-  <div className={style}>
-    <h1>Kebab Studios</h1>
-    <img src='/static/images/highestoffives.gif' />
+  <div>
+    <div className={style}>
+      <h1>Kebab Studios</h1>
+      <img src='/static/images/highestoffives.gif' />
+    </div>
+    <div className={style}>
+      <span className={contact}><a href='mailto:kebab.studios.party.2003@gmail.com' target='_blank' className={peker}>Mail</a></span>
+      <span className={contact}><a href='https://twitter.com/KebabStudios' target='_blank' className={peker}>Twitter</a></span>
+      <span className={contact}><a href='https://itch.io' target='_blank' className={peker}>itch.io</a></span>
+    </div>
   </div>
 )
 
 const style = css({
   textAlign: 'center',
-  fontSize: '5em'
+  fontSize: '4em'
+})
+
+const contact = css({
+  padding: '1em'
+})
+
+const peker = css({
+  color: 'black',
+  textDecoration: 'none'
 })
